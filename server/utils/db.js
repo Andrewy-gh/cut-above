af6cb8741ad500fdc7670a1679e7fe7e9a37fe4a
Andy-git985 <andy.yu617@gmail.com>
2025-10-17 23:52:37 -0400

    Update db production settings

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,7 +2,23 @@ import { Sequelize } from 'sequelize';
 import { DATABASE_URL } from './config.js';
 import logger from './logger/index.js';
 
-export const sequelize = new Sequelize(DATABASE_URL);
+export const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 
 export const connectToDatabase = async () => {
   try {
