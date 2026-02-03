929f5052eb377259a57cae2717899ae973a0bc91
Andy-git985 <andy.yu617@gmail.com>
2026-02-02 23:24:24 -0500

    chore(dev): add dev stop and load client env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,39 +1,42 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dns from 'dns';
 
 dns.setDefaultResultOrder('verbatim');
 
-const convexDeploymentUrl = process.env.CONVEX_DEPLOYMENT_URL ?? '';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'import.meta.env.CONVEX_DEPLOYMENT_URL':
-      JSON.stringify(convexDeploymentUrl),
-  },
-  plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5173,
-    fs: {
-      allow: ['..'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const convexDeploymentUrl = env.CONVEX_DEPLOYMENT_URL ?? '';
+
+  return {
+    define: {
+      'import.meta.env.CONVEX_DEPLOYMENT_URL':
+        JSON.stringify(convexDeploymentUrl),
     },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [react()],
+    server: {
+      host: 'localhost',
+      port: 5173,
+      fs: {
+        allow: ['..'],
+      },
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'mui-x-date-pickers': ['@mui/x-date-pickers'],
-          'mui-x-date-pickers-pro': ['@mui/x-date-pickers-pro'],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'mui-x-date-pickers': ['@mui/x-date-pickers'],
+            'mui-x-date-pickers-pro': ['@mui/x-date-pickers-pro'],
+          },
         },
       },
     },
-  },
+  };
 });
